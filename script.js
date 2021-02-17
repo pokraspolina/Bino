64c31eb041390ef265950f9363c7288f93492a0e
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2021-02-17 13:20:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function openNav() {
           document.getElementById("mySidenav").style.width = "350px";
         }
         
-        function closeNav() {
-          document.getElementById("mySidenav").style.width = "0";
-        }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+$(function() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      $('#topNubex').fadeIn();
+    } else {
+    $('#topNubex').fadeOut();
+    }
+  });
+  $('#topNubex').click(function() {
+    $('body,html').animate({scrollTop:0},700);
+  });
+});
